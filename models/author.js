2756f4a0f5bc2cdffb69c284d0b5d1b1ae2d3f7c
Simbiryan <simbir@inbox.ru>
2019-09-12 21:40:59 +0300

    first commit

---
+++ models/author.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, max: 100},
+    family_name: {type: String, required: true, max: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Виртуальное свойство для полного имени автора
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Виртуальное свойство - URL автора
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  return this.date_of_birth ? 
+    moment(this.date_of_birth).format('DD.MM.YYYY') 
+    : '';
+});
+
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  return this.date_of_death ? 
+    moment(this.date_of_death).format('DD.MM.YYYY') 
+    : '';
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
